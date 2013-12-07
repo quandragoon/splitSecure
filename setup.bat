b34b16efe0bec4cb11f90aebab17c043266c3250
Deepak <deepakn@mit.edu>
2013-12-07 09:55:11 -0500

    Removed binary files

--- setup.bat
+++
@@ -1,6 +0,0 @@
-start python database_server.py 8001
-start python database_server.py 8002
-start python database_server.py 8003
-start python database_server.py 8004
-start python database_server.py 8005
-python auth_server.py
