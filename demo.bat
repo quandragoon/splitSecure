1bf386451ba0756a9609baa3c331e848e3658fef
Pranav <pranavkaundinya@gmail.com>
2013-12-07 19:53:07 -0500

    Fixed bug, cleaned up code

---
+++ demo.bat
@@ -0,0 +1,18 @@
+start python database_server.py 8001
+
+start python database_server.py 8002
+
+start python database_server.py 8003
+
+start python database_server.py 8004
+
+start python database_server.py 8005
+
+start python database_server.py 8006
+
+start python database_server.py 8007
+
+start python database_server.py 8008
+
+python auth_server.py
+
